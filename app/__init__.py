8641a287a4e08f669e4851acc0e6e7005e72a266
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-05-09 17:32:33 -0400

    set up tests/db

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,14 @@ from flask_cors import CORS
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
 
-from app.auth import auth
+#from app.auth import auth
 from app.cli import create_database
-from app.context_processors import utility_text_processors
+#from app.context_processors import utility_text_processors
 from app.db import database
 from app.db import db
 from app.db.models import User
-from app.error_handlers import error_handlers
-from app.logging_config import log_con, LOGGING_CONFIG
+#from app.error_handlers import error_handlers
+#from app.logging_config import log_con, LOGGING_CONFIG
 from app.simple_pages import simple_pages
 
 
@@ -32,7 +32,7 @@ def create_app():
         app.config.from_object("app.config.DevelopmentConfig")
     elif os.environ.get("FLASK_ENV") == "testing":
         app.config.from_object("app.config.TestingConfig")
-    app.mail = Mail(app)
+    #app.mail = Mail(app)
 
     # https://flask-login.readthedocs.io/en/latest/  <-login manager
     login_manager.init_app(app)
@@ -45,16 +45,17 @@ def create_app():
     bootstrap = Bootstrap5(app)
     # these load functions with web interface
     app.register_blueprint(simple_pages)
-    app.register_blueprint(auth)
+    #app.register_blueprint(auth)
     app.register_blueprint(database)
     # these load functionality without a web interface
-    app.register_blueprint(log_con)
-    app.register_blueprint(error_handlers)
-    app.register_blueprint(songs)
-    app.register_blueprint(map)
-    app.context_processor(utility_text_processors)
+    #app.register_blueprint(log_con)
+    #app.register_blueprint(error_handlers)
+    #app.register_blueprint(songs)
+    #app.register_blueprint(map)
+    #app.context_processor(utility_text_processors)
     # add command function to cli commands
     app.cli.add_command(create_database)
+    #app.register_error_handler(404, page_not_found)
     db.init_app(app)
     api_v1_cors_config = {
         "methods": ["OPTIONS", "GET", "POST"],
