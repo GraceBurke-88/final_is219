272b2e31c4a4ec6194d66aa23d3c85056a5839c1
GraceBurke-88 <70772404+GraceBurke-88@users.noreply.github.com>
2022-05-10 16:51:04 -0400

    pytest for songs

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ from app.error_handlers import error_handlers
 from app.logging_config import log_con, LOGGING_CONFIG
 from app.map import map
 from app.simple_pages import simple_pages
-#from app.songs import songs
+from app.songs import songs
 
 #mail = Mail()
 
@@ -53,7 +53,7 @@ def create_app():
     # these load functionality without a web interface
     app.register_blueprint(log_con)
     app.register_blueprint(error_handlers)
-    #app.register_blueprint(songs)
+    app.register_blueprint(songs)
     app.register_blueprint(map)
     app.context_processor(utility_text_processors)
     # add command function to cli commands
